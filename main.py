002a29c780271cec61fc3002bf1611aee34ab0fc
justin-yamamoto <justinyamamoto2@gmail.com>
2022-10-25 17:36:53 -0700

    2nd commit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-print("What is the name of the town you grew up in?")
-town_name = input("Enter Town Name: ")
+print("What is the name of the town you grew up in?\n")
+town_name = input("Enter Town Name: \n")
 
 print("What is the name of your pet?")
-pet_name = input("Enter Pet Name: ")
+pet_name = input("Enter Pet Name: \n")
 
 band_name = town_name + " " + pet_name
 print("Your Band Name: " + band_name)
